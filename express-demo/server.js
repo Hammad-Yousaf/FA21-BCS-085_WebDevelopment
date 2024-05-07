2e7f146683d0a758ea7e9d7cd0f1b7ddfff3da1a
Hammad Yousaf <hammadyousuf87@gmail.com>
2024-05-07 20:05:35 +0500

    ejs
    .

--- express-demo/server.js
+++ express-demo/server.js
@@ -1,14 +1,31 @@
 const express = require("express");
+
 let server = express();
-server.set("view engine","ejs");
+server.use(express.json());
+server.set("view engine", "ejs");
 server.use(express.static("public"));
+var expressLayouts = require("express-ejs-layouts");
+server.use(expressLayouts);
 
-server.get("/contact", function(req,res){    
-    res.render("contact");
-})
-server.get("/", function(req,res){
-   
+
+
+server.get("/contact-us.html", (req, res) => {
+  res.render("contact-us");
+});
+
+server.get("/ajaxapi.html", (req, res) => {
+    res.render("ajaxapi");
+  });
+server.get("/", (req, res) => {
+  res.render("homepage");
+});
+
+server.get("/homepage.html", (req, res) => {
     res.render("homepage");
-})
+  });
 
-server.listen(4000);
+
+
+server.listen(3000, () => {
+  console.log("Server started at localhost:3000");
+});
